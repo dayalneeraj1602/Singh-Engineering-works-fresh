851ee748aa99551de3a203eb509f2171f6357709
Neeraj Dayal <Neeraj.dayal.1602@gmail.com>
2026-01-15 20:49:51 +0530

    added

---
+++ singh-engineering-works-nextjs/src/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{padding: '50px', textAlign: 'center'}}>
+      <h1>Singh Engineering Works</h1>
+      <p>Welcome to our website!</p>
+    </div>
+  )
+}
